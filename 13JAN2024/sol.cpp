9aeab86ebf9cddefa8d779e86ee09d7f170bda1f
DEV REDDY <devreddy4444@gmail.com>
2024-01-14 22:33:26 +0530

    solution added

---
+++ 13JAN2024/sol.cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    Node *insertionSort(struct Node *head_ref)
+    {
+        // code here
+        Node *res = new Node(-1);
+        Node *temp = res;
+        Node *curr = head_ref;
+        while (curr)
+        {
+            temp = res;
+            while (temp->next and curr->data >= temp->next->data)
+            {
+                temp = temp->next;
+            }
+
+            Node *nextNode = temp->next;
+            temp->next = curr;
+            curr = curr->next;
+            temp->next->next = nextNode;
+        }
+
+        return res->next;
+    }
+};
