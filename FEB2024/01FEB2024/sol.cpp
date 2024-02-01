55ae300068b99063334889b2724ba71631f40392
DEV REDDY <devreddy4444@gmail.com>
2024-02-01 21:20:34 +0530

    O(26) solution

---
+++ FEB2024/01FEB2024/sol.cpp
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution
+{
+public:
+    // Function to check if a string is Pangram or not.
+    bool checkPangram(string s)
+    {
+        // your code here
+        vector<int> f(26, 0);
+
+        for (char c : s)
+        {
+            if (isalpha(c))
+            {
+                f[tolower(c) - 'a']++;
+            }
+        }
+
+        for (int count : f)
+        {
+            if (count == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Program to test above functions
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore(INT_MAX, '\n');
+    while (t--)
+    {
+        string s;
+        getline(cin, s);
+        Solution obj;
+        if (obj.checkPangram(s) == true)
+            cout << 1 << endl;
+        else
+            cout << 0 << endl;
+    }
+    return (0);
+}
+
+// } Driver Code Ends
