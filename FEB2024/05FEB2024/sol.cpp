b1840edb9480d2b4583e8891697e40040146eb34
DEV REDDY <devreddy4444@gmail.com>
2024-02-05 23:04:47 +0530

    solution

--- FEB2024/05FEB2024/sol.cpp
+++ FEB2024/05FEB2024/sol.cpp
@@ -37,31 +37,50 @@ class Solution
 public:
     Node *sortedInsert(Node *head, int data)
     {
-        // Your code here
-        Node *prev = nullptr;
-        Node *curr = head;
-        Node *newNode = new Node(data);
-
         if (!head)
         {
-            return newNode;
+            Node *temp = new Node(data);
+            temp->next = temp;
+            return temp;
         }
 
-        while (curr && curr->data < data)
+        Node *h = head->next;
+        Node *pre = head;
+        while (h != head)
         {
-            prev = curr;
-            curr = curr->next;
-        }
-        if (prev)
-        {
-            prev->next = newNode;
-            newNode->next = curr;
-        }
-        if (!prev)
-        {
-            return newNode;
+            pre = h;
+            h = h->next;
         }
 
+        Node *last = pre;
+        h = head;
+        while (1)
+        {
+            if (h->data >= data)
+            {
+                Node *newnode = new Node(data);
+                pre->next = newnode;
+                newnode->next = h;
+
+                if (h == head)
+                    return newnode;
+                else
+                    return head;
+            }
+
+            if (h == last)
+            {
+                Node *newnode = new Node(data);
+                Node *nextnode = h->next;
+                h->next = newnode;
+                newnode->next = nextnode;
+
+                return head;
+            }
+
+            pre = h;
+            h = h->next;
+        }
         return head;
     }
 };
