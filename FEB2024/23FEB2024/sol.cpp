9a3a78b59286795e64505009d5f620b1df96d0bc
DEV REDDY <devreddy4444@gmail.com>
2024-02-23 22:12:26 +0530

    solution

---
+++ FEB2024/23FEB2024/sol.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    // User function Template for C++
+
+    int maxProfit(vector<int> &price)
+    {
+        // Write your code here..
+        int buy1 = INT_MAX, buy2 = INT_MAX,
+            profit1 = 0, profit2 = 0;
+        for (int i = 0; i < price.size(); i++)
+        {
+            buy1 = min(buy1, price[i]);
+            profit1 = max(profit1, price[i] - buy1);
+            buy2 = min(buy2, price[i] - profit1);
+            profit2 = max(profit2, price[i] - buy2);
+        }
+        return profit2;
+    }
+};
