be606cb24f9ad2bc2fce85dc8184f37993d61c94
DEV REDDY <devreddy4444@gmail.com>
2024-03-02 22:41:44 +0530

    o(n) map

---
+++ 03.MARCH2024/02MARCH2024/sol.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    int firstElementKTime(int n, int k, int a[])
+    {
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < n; i++)
+        {
+            map[a[i]]++;
+            if (map[a[i]] == k)
+            {
+                return a[i];
+            }
+        }
+
+        return -1;
+    }
+};
